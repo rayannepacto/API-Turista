536ce021d9ed4811f0b0fc8a7e8daf524778a2f3
--global <--global>
2023-02-09 15:35:47 -0200

    reorganizando

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from atracoes.models import Local
+from comentario.models import Comentario
+from avaliacoes.models import Avaliacao
+from endereco.models import Endereco
+
+
+# Create your models here.
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    local = models.ManyToManyField(Local)
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
